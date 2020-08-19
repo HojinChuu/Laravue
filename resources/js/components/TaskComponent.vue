50fb6d06ac10d81ce0a551619528ed280c76f901
HojinChuu <cnghwls7@gmail.com>
2020-08-19 16:39:08 +0900

    laravel vue

---
+++ resources/js/components/TaskComponent.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <ul class="group-list">
+                <li class="group-list-item"><a href="#">Task 1</a></li>
+                <li class="group-list-item"><a href="#">Task 2</a></li>
+                <li class="group-list-item"><a href="#">Task 3</a></li>
+                <li class="group-list-item"><a href="#">Task 4</a></li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
